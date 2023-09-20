24ad15de1a3d4c95590dd8a9c1d4968aa0b37de7
Edwin Cardenas <eacardenase@unal.edu.co>
2023-09-19 19:06:14 -0500

    feat: add navbar

---
+++ src/app/components/Nav.tsx
@@ -0,0 +1,35 @@
+import Image from 'next/image';
+
+import { headerLogo } from '../../../public/images';
+import { hamburger } from '../../../public/icons';
+import { navLinks } from '@/constants';
+import Link from 'next/link';
+
+const Nav = () => {
+  return (
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <Image src={headerLogo} alt="Logo" width={130} height={29} />
+        </a>
+        <ul className="max-lg:hidden flex flex-1 justify-center items-center gap-16">
+          {navLinks.map(({ href, label }) => (
+            <li key={label}>
+              <Link
+                href={href}
+                className="font-montserrat leading-normal text-lg text-slate-gray"
+              >
+                {label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <div className="max-lg:block hidden">
+          <Image src={hamburger} width={25} height={25} alt="Hamburger" />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,46 @@
-const Home = () => {
-  return (
-    <main>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </main>
-  );
+import { Metadata } from 'next';
+import Nav from './components/Nav';
+import CustomerReviews from './sections/CustomerReviews';
+import Footer from './sections/Footer';
+import Hero from './sections/Hero';
+import PopularProducts from './sections/PopularProducts';
+import Services from './sections/Services';
+import SpecialOffers from './sections/SpecialOffers';
+import Subscribe from './sections/Subscribe';
+import SuperQuality from './sections/SuperQuality';
+
+export const metadata: Metadata = {
+  title: 'Nike',
 };
+
+const Home = () => (
+  <main className="relative">
+    <Nav />
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      <Hero />
+    </section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="padding-x py-10">
+      <Services />
+    </section>
+    <section className="padding">
+      <SpecialOffers />
+    </section>
+    <section className="padding bg-pale-blue">
+      <CustomerReviews />
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
+    </section>
+    <section className="padding-x padding-t pb-8 bg-black">
+      <Footer />
+    </section>
+  </main>
+);
 
 export default Home;
